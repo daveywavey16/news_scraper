3dc7af3529dc7e5b886d5bffde8a77cd5e18c9dd
daveywavey16 <daveywavey16@msn.com>
2018-12-13 19:07:19 -0700

    added missing files

--- server.js
+++ server.js
@@ -27,11 +27,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-//mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
+/*
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://david:norwich12@ds137801.mlab.com:37801/heroku_crs2g898";
 
 mongoose.connect(MONGODB_URI);
+*/
 
 // Routes
 
